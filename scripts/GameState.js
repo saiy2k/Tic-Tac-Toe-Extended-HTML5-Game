b22d31c6d8ca7ebdf93cb283e23f815adf4f8ba4
saiy2k <saiy2k@gmail.com>
2011-06-24 22:05:53 -0700

    1. added manifest file for HTML5 offline to work 2. re written the scoring system and it works right now.

--- scripts/GameState.js
+++ scripts/GameState.js
@@ -22,6 +22,11 @@ along with Zic-Zac-Zoe.  If not, see <http://www.gnu.org/licenses/>.
 	
 	@namespace
 */
+
+var ROW				=	10;
+var COL				=	10;
+var	BLOCK			=	4;
+
 ZicZacZoe.GameState	=	function() {
 
 	var updateCurrentTile			=	function(gBoard, m, clk)
@@ -53,15 +58,15 @@ ZicZacZoe.GameState	=	function() {
 				t.tiles[t.selectedTileY][t.selectedTileX] =   t.currentPlayerID;
 			}
 			
-			for (var j = 0; j < 10; j++)
+			for (var j = 0; j < ROW; j++)
 			{
 				var str = '';
-				for (var  i = 0; i < 10; i++)
+				for (var  i = 0; i < COL; i++)
 				{
 					var til	=	ZicZacZoe.GameState.tiles[j][i];
 					str = str + ', ' + til;
 				}
-				//console.log(str);
+				console.log(str);
 			}
 		}
 	};
@@ -70,72 +75,109 @@ ZicZacZoe.GameState	=	function() {
 	{
 		var t			=	ZicZacZoe.GameState;
 		var	p			=	t.currentPlayerID;
-		var giveScore;
-		var	tmpScore	=	0;
 		
 		
-		giveScore		=	true;
-		for ( var i = t.selectedTileX; i >= 0 && i > t.selectedTileX - 5 ; i-- )
+		var	p1			=	0;
+		var	p2			=	0;
+		
+		
+		for (var j = 0; j < ROW; j++)
 		{
-			if ( t.tiles[t.selectedTileY][i] != p )
+			for (var i = 0; i < COL; i++)
 			{
-				giveScore	=	false;
-				break;
+				var			cVal;
+				var			cnt;
+				cVal	=	t.tiles[j][i];
+				
+				//check if the current tile is not blank
+				if (cVal != -1)
+				{
+					//count the number of 0s and 1s in the next 'BLOCK' tiles in the current row
+					cnt	=	0;
+					for (var z = i; z < i + BLOCK && z < COL; z++)
+					{
+						if (0 == t.tiles[j][z])
+							cnt++;
+						else if(1 == t.tiles[j][z])
+							cnt--;
+						else
+							break;
+					}
+					if (cnt == BLOCK)		//if there were 'BLOCK' consecutive tiles with value 0, this would have been true
+						p1++;
+					if (cnt == -BLOCK)		//if there were 'BLOCK' consecutive tiles with value 1, this would have been true
+						p2++;
+					
+					
+					//count the number of 0s and 1s in the next 'BLOCK' tiles in the current column
+					cnt	=	0;
+					for (var z = j; z < j + BLOCK && z < ROW; z++)
+					{
+						if (0 == t.tiles[z][i])
+							cnt++;
+						else if(1 == t.tiles[z][i])
+							cnt--;
+						else
+							break;
+					}
+					if (cnt == BLOCK)
+						p1++;
+					if (cnt == -BLOCK)
+						p2++;
+						
+					
+					//count the number of 0s and 1s in the straight diagonal
+					cnt	=	0;
+					for (var z = 0; z < BLOCK; z++)
+					{
+						if ( z+j >= COL || z+i >= ROW )
+							break;
+							
+						if (0 == t.tiles[z+j][z+i])
+							cnt++;
+						else if(1 == t.tiles[z+j][z+i])
+							cnt--;
+						else
+							break;
+					}
+					if (cnt == BLOCK)
+						p1++;
+					if (cnt == -BLOCK)
+						p2++;
+						
+					
+					//count the number of 0s and 1s in the reverse diagonal
+					cnt	=	0;
+					for (var z = 0; z < BLOCK; z++)
+					{
+						if ( j+z >= COL || i-z < 0 )
+							break;
+							
+						if (0 == t.tiles[j+z][i-z])
+							cnt++;
+						else if(1 == t.tiles[j+z][i-z])
+							cnt--;
+						else
+							break;
+					}
+					if (cnt == BLOCK)
+						p1++;
+					if (cnt == -BLOCK)
+						p2++;
+				}
 			}
 		}
-		if ( giveScore )
-			tmpScore++;
 		
 		
-		giveScore		=	true;
-		for ( var i = t.selectedTileX; i < 10 && i < t.selectedTileX + 5 ; i++ )
-		{
-			if ( t.tiles[t.selectedTileY][i] != p )
-			{
-				giveScore	=	false;
-				break;
-			}
-		}
-		if ( giveScore )
-			tmpScore++;
-		
-		
-		giveScore		=	true;
-		for ( var j = t.selectedTileY; j < 10 && j < t.selectedTileY + 5 ; j++ )
-		{
-			if ( t.tiles[j][t.selectedTileX] != p )
-			{
-				giveScore	=	false;
-				break;
-			}
-		}
-		if ( giveScore )
-			tmpScore++;
-			
-			
-		giveScore		=	true;
-		for ( var j = t.selectedTileY; j >= 0 && j > t.selectedTileY - 5 ; j-- )
-		{
-			if ( t.tiles[j][t.selectedTileX] != p )
-			{
-				giveScore	=	false;
-				break;
-			}
-		}
-		if ( giveScore )
-			tmpScore++;
-		
-		if(p == 0)
-			t.player1Score	+=	tmpScore;
-		else
-			t.player2Score	+=	tmpScore;
+		t.player1Score	=	p1;
+		t.player2Score	=	p2;
 	};
 	
 	var endTurn						=	function()
 	{
 		var t			=	ZicZacZoe.GameState;
 		
-		if( t.currentPlayerID == 0 )
+		if( t.currentPlayerID	== 0 )
 			t.currentPlayerID	=	1;
 		else
 			t.currentPlayerID	=	0;
